65a53caa04d40acf91ad2015cfbe75cd01fe82d8
Maksim Netesov <MrEasters@mail.ru>
2024-11-08 21:19:41 +0300

    fix settings for .env

--- HabitTracker/settings.py
+++ HabitTracker/settings.py
@@ -63,10 +63,11 @@ WSGI_APPLICATION = "HabitTracker.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
-        "HOST": "db"
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT")
     }
 }
 
